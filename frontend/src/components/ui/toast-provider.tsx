15f79715fe2488c59617724fcb59353caa280696
Stephanie Tan <stephanietan@Stephanies-MacBook-Pro.local>
2025-11-27 11:34:39 -0500

    Move toast notifications to bottom-right

--- frontend/src/components/ui/toast-provider.tsx
+++ frontend/src/components/ui/toast-provider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "sonner";
 export function ToastProvider() {
   return (
     <Toaster
-      position="top-right"
+      position="bottom-right"
       expand={false}
       richColors
       closeButton
